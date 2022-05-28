05805a81e000ad03954f2f4a046c659235878ea7
PokeTideBots <103623854+PokeTideBots@users.noreply.github.com>
2022-05-28 09:34:48 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -1,15 +1,15 @@
 #Don't be a thief by stealing other's Hardwork, it took time and effort to make this repo.
 #Respect my work by not making any changes here.
 
-START_TEXT = "Send me any file to begin."
+START_TEXT = "Send me any file to begin.🗂️ I am Do 5 Process.🔥 View in below List.🚧\n\n What's New? \n\n✪ Encode \n✪ Compressor \n✪ Convert \n✪ Rename \n✪ Screenshot \n✪ Trim \n\n✨ Powered by 💠 @PokeTide 💠"
 
 FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
 
-CHANNEL_LINK = "https://t.me/DroneBots"
+CHANNEL_LINK = "https://t.me/PokeTide"
 
-SUPPORT_LINK = "https://t.me/TeamDrone"
+SUPPORT_LINK = "https://t.me/Pokemon_Group_Telegram"
 
-info_text = "This bot is developed by @MaheshChauhan\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.4"   
+info_text = "This bot is developed by @PokeRequest_Bot\n\n📱 Written in python library TELETHON.\n\n〽️ Bot by : @PokeTide\n\n🌻 Support : @Pokemon_Group_Telegram\n\n🔗 V1.4"   
 
 help_text = """**v1.4**
 
@@ -29,13 +29,13 @@ help_text = """**v1.4**
 
 source_text = "**Deploy your own bot**"
 
-DEV = "https://t.me/MaheshChauhan"
+DEV = "https://t.me/PokeTide"
 
 spam_notice = "This bot is hosted on heroku, and hence can just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
 
 JPG = "LOCAL/video_convertor.jpg"
 
-JPG0 = 'https://telegra.ph/file/d98c559b56ef884ef3bad.jpg'
+JPG0 = 'https://telegra.ph/file/96a8e440ca77fbf5b4c67.jpg'
 
 JPG2 = "LOCAL/20211215_165751.jpg"
 
