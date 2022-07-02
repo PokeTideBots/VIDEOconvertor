1dd5317fd594fdf7037115823306d6d654d87506
PokeTideBots <103623854+PokeTideBots@users.noreply.github.com>
2022-07-02 17:21:55 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -7,16 +7,16 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-BOT_UN = config("BOT_UN", default=None)
-AUTH_USERS = config("AUTH_USERS", default=None, cast=int)
-LOG_CHANNEL = config("LOG_CHANNEL", default=None)
-LOG_ID = config("LOG_ID", default=None)
-FORCESUB = config("FORCESUB", default=None)
-FORCESUB_UN = config("FORCESUB_UN", default=None)
-ACCESS_CHANNEL = config("ACCESS_CHANNEL", default=None)
-MONGODB_URI = config("MONGODB_URI", default=None)
+API_ID= 17124006
+API_HASH= "97e184dc9c07513de148002859aef2b2"
+BOT_TOKEN= "5362862681:AAHksypiJVa_UZV78rZqOaGFBDu4BPK-fVg"
+BOT_UN= "PokeTideVideoConverterBot"
+LOG_CHANNEL= "PokeTideUploads"
+ACCESS_CHANNEL= -1001409710934
+AUTH_USERS= 1165699179
+MONGODB_URI= "mongodb+srv://Mailmirror:fake100@cluster0.5ystf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+FORCESUB= -1001726913223
+FORCESUB_UN= "PokeTide"
+LOG_ID= -1001645644921
 
 Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
